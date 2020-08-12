19c508d65a431e81e81ae0bea45cd72cfe4084a9
gongjian <gongjian@zycfcinc.com>
2020-08-12 10:50:27 +0800

    feat: plop 模板生成器

---
+++ plop-react/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+export default () => {
+  <div className='Footer'>
+    Footer Component
+  </div>
+}
---
+++ plop-react/src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+
+export default () => {
+  <div className='Header'>
+    Header Component
+  </div>
+}
